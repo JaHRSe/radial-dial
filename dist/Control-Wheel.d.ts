53aa3756eacd7c85d24e061f5fa3d780408e44bc
Jake <jacob.searles.3@us.af.mil>
2023-03-13 05:56:16 -0400

    new build

--- dist/Control-Wheel.d.ts
+++ dist/Control-Wheel.d.ts
@@ -3,6 +3,16 @@ type radialProps = {
     circleThick: number;
     padding: number;
     backGroundColor?: string;
+    continuous?: boolean;
 };
+/**Returns an HTML canvas element with a graphical wheel and numerical angle display inside of the wheel.
+ * As the wheel rotates the display is updated. with the new angle in degrees. Fires even on
+ * mouseup or optionally on mouse move with the angle value in radians
+ * @param {number} radius - The radius of the circle in pixels
+ * @param {number} circleThick - The thickness of the wheel in pixels
+ * @param {number} padding - The padding space around the wheel in pixels
+ * @param {string} [backgroundColor] - The color of the canvas behind the wheel
+ * @param {string} [continuous] - If true, generate rotate event continuously as user moves the wheel
+ *  */
 export declare function ControlWheel(props: radialProps): HTMLCanvasElement;
 export {};
